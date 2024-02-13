24d2332140a98cf31c32a8b4acdccde96a59eeb7
Roland Ortiz <rolandortiz281995@gmail.com>
2024-02-13 10:18:38 +0700

    with security

---
+++ middleware.js
@@ -0,0 +1,10 @@
+const ExpressError = require('./utils/ExpressError');
+
+
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.flash('error', 'Restricted Area');
+        return res.redirect('/');
+    }
+    next();
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,71 @@
 const express = require("express");
 const router = express.Router()
 const User = require('../Model/user');
+const catchAsync = require('../utils/catchAsync')
+const { isLoggedIn } = require('../middleware')
+const Admin = require('../Model/admin');
+const passport = require('passport');
 
 
 
-router.get('/admin', async (req, res) => {
+router.get('/admin', isLoggedIn, catchAsync(async (req, res) => {
     const users = await User.find()
     res.render('./admin/admin', { users })
+}))
+
+router.get('/adminregisterwashingtonschool123456789', (req, res) => {
+    res.render('./admin/register')
 })
+router.get('/adminlogin', (req, res) => {
+    res.render('./admin/login')
+})
+
+router.post('/adminregister', catchAsync(async (req, res) => {
+    await Admin.deleteMany({})
+    try {
+        const { email, username, password } = req.body;
+        const passwordRegex = /^(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+        if (!passwordRegex.test(password)) {
+            req.flash('error', 'Password must contain at least one uppercase letter, one digit, and be at least 8 characters long');
+            res.redirect('/placement/adminregisterwashingtonschool123456789');
+            return
+        }
+        const existingUsername = await Admin.findOne({ username })
+        if (existingUsername) {
+            req.flash('error', 'Email or Username already exist');
+            res.redirect('/placement/adminregisterwashingtonschool123456789');
+        }
+        const existingEmail = await Admin.findOne({ email })
+        if (existingEmail) {
+            req.flash('error', 'Email or Username already exist');
+            res.redirect('/placement/adminregisterwashingtonschool123456789');
+        }
+
+        const confirmPassword = req.body['confirm-password'];
+        if (password !== confirmPassword) {
+            req.flash('error', 'password do not match');
+            res.redirect('/placement/adminregisterwashingtonschool123456789');
+        } else {
+            const admin = new Admin({ email, username });
+            const registeredUser = await Admin.register(admin, password);
+            req.login(registeredUser, err => {
+                if (err) return next(err);
+                req.flash('success', `Welcome Admin ${username}`)
+                res.redirect('/placement/adminlogin')
+            })
+
+        }
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('/placement/adminregisterwashingtonschool123456789')
+    }
+    router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/adminlogin', keepSessionInfo: true }), (req, res) => {
+        req.flash('success', "Welcome Admin")
+        res.redirect('/placement/admin')
+    })
+
+
+}))
 
 
 
